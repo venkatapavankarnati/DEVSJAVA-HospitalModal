3d5f6ab5cdc07fb9c9c95f59d3ad5cb60a899950
Venkata Pavan Karnati <43650938+venkatapavankarnati@users.noreply.github.com>
2021-02-04 15:41:36 -0500

    testing (#1)
    * testing
    
    * resolved error
    
    * testing #3
    
    * testing #4
    
    * testing #5
    
    * testing #6
    
    * testing #6
    
    * testing #7
    
    * testing #8
    
    * testing #9
    
    * testing #10
    
    * testing #10
    
    * testing #10
    
    * testing #10
    
    * testing #10
    
    * testing #10
    
    * testing #10
    
    * final commit
    
    * final commit

--- Doctor.java
+++ Doctor.java
@@ -1,69 +1,68 @@
 package HospitalModel;
-import simView.*;import java.lang.*;
+
+import simView.*;
+import java.lang.*;
 import genDevs.modeling.*;
 import genDevs.simulation.*;
 import GenCol.*;
 import util.*;
 import statistics.*;
 
-
 public class Doctor extends ViewableAtomic {
 	protected int activeTime = 20;
-	protected int count =0;
+	protected int count = 0;
 	protected String currPatient;
 	entity message;
-	public Doctor()
-	{
+
+	public Doctor() {
 		this("Doctor");
 	}
-	
-	public Doctor(String name){
-		   super(name);
-		   addInport("IN");
-		   addOutport("OUT");
-		}
-	
-	public void initialize(){
-		   holdIn("passive", INFINITY);
-		   count++;
-		}
-	
-	public void  deltext(double e,message x)
-	{
-		double rand = (Math.random()*10)+10;
-	   Continue(e);
-	   for (int i=0; i< x.getLength();i++){
-		   message = x.getValOnPort("IN",i);
-	     if (messageOnPort(x, "IN", i)) {
-	    	 currPatient = message.getName();
-	    	 holdIn("Active",rand);
-	     }
-	   }
-	}
-	
-	public void  deltint( )
-	{
 
-	if(phaseIs("active")){
-	   count++;
-	   holdIn("passive", INFINITY);
+	public Doctor(String name) {
+		super(name);
+		addInport("IN");
+		addOutport("OUT");
 	}
-	else passivate();
+
+	public void initialize() {
+		holdIn("passive", INFINITY);
+		count++;
 	}
-	
-	public message  out( )
-	{
-	System.out.println(" OUT count "+count);
-	   message  m = new message();
-	   content con;
-	   if(name.equals("DoctorOne"))
-			 con = makeContent("OUT", new entity("DoctorOne"+currPatient));
-	   else if(name.equals("DoctorTwo"))
-		     con = makeContent("OUT", new entity("DoctorTwo"+currPatient));
-	   else
-		   con = makeContent("OUT", new entity("DoctorThree"+currPatient));
-	   m.add(con);
-	   return m;
+
+	public void deltext(double e, message x) {
+		double rand = (Math.random() * 10) + 10;
+		Continue(e);
+		for (int i = 0; i < x.getLength(); i++) {
+			message = x.getValOnPort("IN", i);
+			if (messageOnPort(x, "IN", i)) {
+				currPatient = message.getName();
+				System.out.print(currPatient);
+				holdIn("Active", rand);
+			}
+		}
 	}
-	
+
+	public void deltint() {
+
+		if (phaseIs("active")) {
+			count++;
+			holdIn("passive", INFINITY);
+		} else
+			passivate();
+	}
+
+	public message out() {
+		System.out.println(" OUT count " + count);
+		message m = new message();
+		content con;
+		if (name.equals("DoctorOne"))
+			con = makeContent("OUT", new entity("DoctorOne" + currPatient));
+		else if (name.equals("DoctorTwo"))
+			con = makeContent("OUT", new entity("DoctorTwo" + currPatient));
+		else
+			con = makeContent("OUT", new entity("DoctorThree" + currPatient));
+		m.add(con);
+		return m;
+	}
+
 }
