97737ecfac333b34acd397f8307414c46991679e
venkatapavankarnati <pavan.karnati97@gmail.com>
2021-03-12 16:50:39 -0500

    added stuff

---
+++ testagain/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+$fileLocation = Join-Path $toolsDir 'test.zip'
+$dest = 'C:\Users\pavan\Documents\efc'
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  Destination  = $dest
+  FileFullPath = $fileLocation
+}
+Get-ChocolateyUnzip @packageArgs
